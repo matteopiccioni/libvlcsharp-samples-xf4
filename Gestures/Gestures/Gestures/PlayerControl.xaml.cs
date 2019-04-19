7b8950a39945bd39c6300d468a9d8c36ab43a81e
Matteo <matteo.piccioni@inim.biz>
2019-04-19 14:35:44 +0200

    First commit of Gestures examples with xamarin form 4 exception on back button

---
+++ Gestures/Gestures/Gestures/PlayerControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace Gestures
+{
+    public partial class PlayerControl : ContentPage
+    {
+        MainViewModel _vm;
+
+        public PlayerControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            videoView.MediaPlayerChanged += MediaPlayerChanged;
+
+            _vm = BindingContext as MainViewModel;
+            _vm.Initialize();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            videoView.MediaPlayerChanged -= MediaPlayerChanged;
+
+            _vm = BindingContext as MainViewModel;
+            _vm.Stop();
+        }
+
+        private void MediaPlayerChanged(object sender, System.EventArgs e)
+        {
+            _vm.MediaPlayer.Play();
+        }
+        
+        void PanUpdated(object sender, PanUpdatedEventArgs e) => _vm.OnGesture(e);
+    }
+}
